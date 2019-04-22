3af94988fae6dba15c8908a724f861ede5b9088c
jinchuanchuan <sdzzjcc@163.com>
2019-04-22 21:32:14 +0800

    添加命令模式

---
+++ src/main/java/command/Invoker.java
@@ -0,0 +1,13 @@
+package command;
+
+public class Invoker {
+    private Command command;
+
+    public void setCommand(Command command) {
+        this.command = command;
+    }
+
+    public void action() {
+        this.command.execute();
+    }
+}
